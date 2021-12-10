5f4a01f81e7fa7c812bb6b4544a7e1d62018f445
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2021-12-10 18:47:18 +0000

    adding extra files

---
+++ firebase_details.py
@@ -0,0 +1,14 @@
+def link_to_firebase():
+    firebase_config = {
+        "apiKey": "AIzaSyCbfuvreUeRFO11cwrcOCKrd6v_xE5dGEw",
+        "authDomain": "multi-dimensional-cj.firebaseapp.com",
+        "databaseURL": "https://multi-dimensional-cj-default-rtdb.europe-west1.firebasedatabase.app",
+        "projectId": "multi-dimensional-cj",
+        "storageBucket": "multi-dimensional-cj.appspot.com",
+        "messagingSenderId": "406315615131",
+        "appId": "1:406315615131:web:84ba8240b63a015e7c973e",
+        "measurementId": "G-9FWWWLTX60"
+    }
+    
+    return firebase_config
+
