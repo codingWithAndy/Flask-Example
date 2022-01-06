310aa0eeaa4476ac858d79605be844c5f6007d9c
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2022-01-06 17:25:43 +0000

    corrected JSON error.

--- application.py
+++ application.py
@@ -264,5 +264,5 @@ def logout():
 
     return redirect(url_for("index"))
 
-#if __name__ == '__main__':
-#    application.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
+if __name__ == '__main__':
+    application.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
